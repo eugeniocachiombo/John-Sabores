a980410461746ff5287d9cb5239e3a7115bb2231
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-11 15:51:19 +0100

    Retificação dos arquivos PDF que não estavão a ser reconhecidos

---
+++ assets/js/modo_escuro_claro.js
@@ -0,0 +1,33 @@
+let modoEscuro = document.querySelector("#modoEscuro");
+    let modoClaro = document.querySelector("#modoClaro");
+
+
+    function MudarCorFundo(){
+        if(modoClaro.style.display == "block"){
+            modoEscuro.style.display = "block";
+            modoClaro.style.display = "none";
+            document.body.style = "font-size: 20px;background: #222;color: white;font-family: verdana;";
+            localStorage.setItem("modo", "modoEscuro");
+        } else {
+            modoClaro.style.display = "block";
+            modoEscuro.style.display = "none";
+            document.body.style = "font-size: 20px;background: white;color: black;font-family: verdana;";
+            localStorage.setItem("modo", "modoClaro");
+        } 
+    } 
+
+    setInterval(() => {
+        if(localStorage.getItem("modo") != null){
+            if(localStorage.getItem("modo") == "modoEscuro"){
+                modoEscuro.style.display = "block";
+                modoClaro.style.display = "none";
+                document.body.style = "font-size: 20px;background: #222;color: white;font-family: verdana;";
+                //localStorage.clear("modo");
+            } else {
+                modoClaro.style.display = "block";
+                modoEscuro.style.display = "none";
+                document.body.style = "font-size: 20px;background: white;color: black;font-family: verdana;";
+                //localStorage.clear("modo");
+            } 
+        }
+    }, 1000);
