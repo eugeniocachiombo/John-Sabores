ea2277288ac4b59358a9913d42d0d790cf469347
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-10-08 22:34:00 +0100

    feat: create login page

--- routes/user/routes.php
+++ routes/user/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\User\AdminDashboard;
+use App\Livewire\User\Login;
 use App\Livewire\Web\LandingPage;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix("administrador")->name("admin.")->group(function(){
     Route::get("/painel", AdminDashboard::class)->name("dashboard");
 });
+
+Route::get("/login", Login::class)->name("login");
+
