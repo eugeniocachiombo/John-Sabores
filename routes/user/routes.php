8cc4522d2992e2676080871fa7d9cc03262a1f70
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-10-10 13:24:32 +0100

    feat: middleware implemented

--- routes/user/routes.php
+++ routes/user/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\DontRequireLogin;
+use App\Http\Middleware\RequireLogin;
 use App\Livewire\User\AdminDashboard;
 use App\Livewire\User\Login;
 use App\Livewire\Web\LandingPage;
@@ -8,7 +10,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix("administrador")->name("admin.")->group(function(){
     Route::get("/painel", AdminDashboard::class)->name("dashboard");
-});
+})->middleware(RequireLogin::class);
 
-Route::get("/login", Login::class)->name("login");
+Route::get("/login", Login::class)->name("login")
+->middleware(DontRequireLogin::class);
 
