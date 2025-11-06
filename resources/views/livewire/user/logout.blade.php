d3130de645dfbf3328625c6458d90b2b317be1d1
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-11-06 12:45:06 +0100

    feat: branch feature mix database is ready to deploy

--- resources/views/livewire/user/logout.blade.php
+++ resources/views/livewire/user/logout.blade.php
@@ -1,3 +1,3 @@
 <div>
-   <a href="#" wire:click.prevent="exit" class="nav-link text-danger"><i class="fa fa-sign-out-alt me-2"></i> Sair</a>
+   <a href="#" wire:click.prevent="exit" class="nav-link text-danger"><i class="fa fa-sign-out-alt me-2 "></i> Sair</a>
 </div>
