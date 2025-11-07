833a3025a8febb1815ff441e364f952667afa7df
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-11-07 07:18:05 +0100

    perf: instalar composer por routa

--- public/install_composer.php
+++
@@ -1,3 +0,0 @@
-<?php
-shell_exec('composer install --no-dev --optimize-autoloader');
-echo "Composer dependencies installed.";
--- routes/web/routes.php
+++ routes/web/routes.php
@@ -7,6 +7,11 @@ Route::get("/", function (){
     return redirect()->route("web.home");
 });
 
+Route::get("/install_composer", function (){
+    shell_exec('composer install --no-dev --optimize-autoloader');
+    echo "Composer dependencies installed.";
+});
+
 
 Route::prefix("web")->name("web.")->group(function(){
     Route::get("/inicio", LandingPage::class)->name("home");
