3d1a1ba2394bade808ae4d48518cac959546dd08
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-11-07 07:23:45 +0100

    perf: removido routa de instalar composer

--- routes/web/routes.php
+++ routes/web/routes.php
@@ -7,12 +7,6 @@ Route::get("/", function (){
     return redirect()->route("web.home");
 });
 
-Route::get("/install_composer", function (){
-    shell_exec('composer install --no-dev --optimize-autoloader');
-    echo "Composer dependencies installed.";
-});
-
-
 Route::prefix("web")->name("web.")->group(function(){
     Route::get("/inicio", LandingPage::class)->name("home");
 });
